57c8b6cd57fb9409ee115c000924a5e69cf9aca3
zhai-php <455078389@qq.com>
2014-07-23 15:24:16 +0800

    2

---
+++ discuz/upload/uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://127.0.0.1/qujianshenla/discuz/upload',
+    'authkey' => '99n3M4hdIade23n7v095o9H2feva8cA2A72aI2NbG9t6w0ldM0neh7qbc0cfZ2af',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
